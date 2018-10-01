0b0959534951dbfb4115d2d96ce3a9b1d1c2221c
Mateusz Midor <mateusz.midor@aptiv.com>
2018-10-01 07:16:35 +0200

    Added build_shared_and_run.sh & build_static_and_run.sh to image example

---
+++ src/github.com/mateuszmidor/GoStudy/image/build_static_and_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Building golang libimgutil.a library..."
+go build -o libimgutil.a -buildmode=c-archive -ldflags="-s -w" github.com/mateuszmidor/GoStudy/image/main/
+
+echo "Building c++ test app..."
+g++ test.cpp -o test_static -static -L . -limgutil -pthread
+
+echo "Running test app with ball.jpg param..."
+./test_static ball.jpg
