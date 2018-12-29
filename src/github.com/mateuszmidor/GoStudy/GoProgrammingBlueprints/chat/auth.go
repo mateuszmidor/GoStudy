cc74b2309807b421c1ae63fec8cdc5fe895ecb8e
Mateusz Midor <3demaniac@gmail.com>
2018-12-29 13:15:00 +0100

    Added login support to chat

---
+++ src/github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/chat/auth.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/stretchr/objx"
+
+	"github.com/stretchr/gomniauth"
+)
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, err := r.Cookie("auth")
+	if err == http.ErrNoCookie {
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// success
+	h.next.ServeHTTP(w, r)
+}
+
+// MustAuth is a decorator that requires authentication before handling Http request
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
+
+func renderError(w http.ResponseWriter, msg string) {
+	w.Write([]byte(msg))
+}
+
+// format: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	segs := strings.Split(path, "/")
+
+	if len(segs) < 4 {
+		msg := "Invalid url: " + path
+		renderError(w, msg)
+		return
+	}
+
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error fetching provider %s:%s", provider, err), http.StatusBadRequest)
+			return
+		}
+		loginURL, err := provider.GetBeginAuthURL(nil, nil)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("GetBeginAuthURL for %s:%s error", provider, err), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", loginURL)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+
+	case "callback":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error fetching provider %s:%s", provider, err), http.StatusBadRequest)
+			return
+		}
+		creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error authenticating for %s:%s", provider, err), http.StatusInternalServerError)
+			return
+		}
+		user, err := provider.GetUser(creds)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error fetching user from %s:%s", provider, err), http.StatusInternalServerError)
+			return
+		}
+		authCookieValue := objx.New(map[string]interface{}{
+			"name": user.Name()}).MustBase64()
+
+		http.SetCookie(w, &http.Cookie{Name: "auth", Value: authCookieValue, Path: "/"})
+		w.Header().Set("Location", "/chat")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Auth using %s not supported", action)
+	}
+
+}
--- src/github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/chat/main.go
+++ src/github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/chat/main.go
@@ -9,6 +9,13 @@ import (
 	"sync"
 	"text/template"
 
+	"github.com/stretchr/objx"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+
 	"github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/trace"
 )
 
@@ -22,7 +29,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
 }
 
 func newRoom() *room {
@@ -35,13 +48,37 @@ func newRoom() *room {
 	}
 }
 
-// need: go get github.com/gorilla/websocket
+// https://console.developers.google.com/apis/credentials?project=api-project-44918022082 :
+//Twój identyfikator klienta
+//v44918022082-b07tui5r5ud2snbe8ur7ag41qta643ng.apps.googleusercontent.com
+//Twój tajny klucz klienta
+//H3vEKMxxJu-tQjwcE4axM62q
+
+func initOAuth2() {
+	gomniauth.SetSecurityKey("AUTH_KEY")
+	gomniauth.WithProviders(google.New("44918022082-b07tui5r5ud2snbe8ur7ag41qta643ng.apps.googleusercontent.com", "H3vEKMxxJu-tQjwcE4axM62q", "http://localhost:8080/auth/callback/google"),
+		facebook.New("id", "pass", "http://localhost:8080/auth/callback/facebook"),
+		github.New("id", "pass", "http://localhost:8080/auth/callback/github"),
+	)
+}
+
+// need:
+// go get github.com/gorilla/websocket
+// go get github.com/stretchr/gomniauth
+// go get github.com/clbanning/x2j
+// go get github.com/ugorji/go/codec
+// go get gopkg.in/mgo.v2/bson
+// setup google OAuth client ID under https://console.developers.google.com/apis/credentials?project=api-project-44918022082
+// and put the id and pass in "initOAuth2" function
 func main() {
 	var addr = flag.String("addr", ":8080", "Server http address")
 	flag.Parse()
+	initOAuth2()
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	go r.run()
 	log.Println("Running the server at", *addr)
