be096c1a07524a2cc09fbc4ea109ff916a2065c5
Mateusz Midor <3demaniac@gmail.com>
2019-01-07 22:59:09 +0100

    socialpoll: publishing votes to NSQ messaging system

--- src/github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/socialpoll/twittervotes/twittervotes.go
+++ src/github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/socialpoll/twittervotes/twittervotes.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+
+	nsq "github.com/bitly/go-nsq"
 
 	mgo "gopkg.in/mgo.v2"
 )
@@ -28,6 +32,30 @@ func loadOptions() ([]string, error) {
 	}
 	iter.Close()
 	return options, iter.Err()
+}
+
+func exitOnError(err error) {
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func publishVotes(votes <-chan string) <-chan struct{} {
+	stopchan := make(chan struct{}, 1)
+	pub, err := nsq.NewProducer("localhost:4150", nsq.NewConfig())
+	exitOnError(err)
+	go func() {
+		for vote := range votes {
+			err := pub.Publish("votes", []byte(vote))
+			exitOnError(err)
+		}
+		log.Println("Producer: stopping...")
+		pub.Stop()
+		log.Println("Producer: stopped")
+		stopchan <- struct{}{}
+	}()
+	return stopchan
 }
 func main() {
 
