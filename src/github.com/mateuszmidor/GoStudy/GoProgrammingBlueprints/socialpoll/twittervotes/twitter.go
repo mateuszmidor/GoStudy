f6b34e34ad5090d3fb7c303e266e4e1d75a9ea2f
Mateusz Midor <3demaniac@gmail.com>
2019-01-07 21:29:50 +0100

    socialpoll: reading tweets from Twitter

--- src/github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/socialpoll/twittervotes/twitter.go
+++ src/github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/socialpoll/twittervotes/twitter.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net"
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -87,4 +89,69 @@ func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
 	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
 	req.Header.Set("Authorization", authClient.AuthorizationHeader(creds, "POST", req.URL, params))
 	return httpClient.Do(req)
+}
+
+type tweet struct {
+	Text string
+}
+
+func readFromTwitter(votes chan<- string) {
+	options, err := loadOptions()
+	if err != nil {
+		log.Println("Couldnt load options: ", err)
+		return
+	}
+	u, err := url.Parse("https://stream.twitter.com/1.1/statuses/filter.json")
+	if err != nil {
+		log.Println("Couldnt create filter request: ", err)
+		return
+	}
+	query := make(url.Values)
+	query.Set("track", strings.Join(options, ","))
+	req, err := http.NewRequest("POST", u.String(), strings.NewReader(query.Encode()))
+	if err != nil {
+		log.Println("Couldnt create filter request: ", err)
+		return
+	}
+	resp, err := makeRequest(req, query)
+	if err != nil {
+		log.Println("Couldnt make request: ", err)
+		return
+	}
+	reader := resp.Body
+	decoder := json.NewDecoder(reader)
+	for {
+		var t tweet
+		if err := decoder.Decode(&t); err != nil {
+			break
+		}
+		for _, option := range options {
+			if strings.Contains(strings.ToLower(t.Text), strings.ToLower(option)) {
+				log.Println("vote: ", option)
+				votes <- option
+			}
+		}
+	}
+}
+
+func startTwitterStream(stopchan <-chan struct{}, votes chan<- string) <-chan struct{} {
+	stoppedChan := make(chan struct{}, 1)
+	go func() {
+		defer func() {
+			stoppedChan <- struct{}{}
+		}()
+		for {
+			select {
+			case <-stopchan:
+				log.Println("Closing twitter connection...")
+				return
+			default:
+				log.Println("Searching twitter...")
+				readFromTwitter(votes)
+				log.Println(" (waiting...)")
+				time.Sleep(10 * time.Second)
+			}
+		}
+	}()
+	return stopchan
 }
--- src/github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/socialpoll/twittervotes/twittervotes.go
+++ src/github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/socialpoll/twittervotes/twittervotes.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"log"
+
+	mgo "gopkg.in/mgo.v2"
+)
+
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("dialing mongodb: localhost")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
 func main() {
 
 }
