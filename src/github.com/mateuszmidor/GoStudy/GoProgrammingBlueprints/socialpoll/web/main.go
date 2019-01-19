378c34cecb309fbf5d9f24e700ccca75928f4dcf
Mateusz Midor <3demaniac@gmail.com>
2019-01-19 12:07:55 +0100

    Added socialpoll web server to present polls as web page. Almost works

---
+++ src/github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/socialpoll/web/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	var addr = flag.String("addr", ":8081", "Web page address")
+	flag.Parse()
+	mux := http.NewServeMux()
+	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("public"))))
+	log.Println("Running web server at:", *addr)
+	http.ListenAndServe(*addr, mux)
+}
