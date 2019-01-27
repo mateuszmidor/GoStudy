6bb0f5bc8264c326110b733a936b05af96f6d954
Mateusz Midor <3demaniac@gmail.com>
2019-01-27 19:38:25 +0100

    Meander API works - retrieves recommended places for query

--- src/github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/meander/cmd/meander/main.go
+++ src/github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/meander/cmd/meander/main.go
@@ -2,15 +2,44 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/meander"
 )
 
 func main() {
-	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/journeys", cors(func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
-	})
+	}))
+	http.HandleFunc("/recommendations", cors(func(w http.ResponseWriter, r *http.Request) {
+		uq := r.URL.Query()
+		q := &meander.Query{
+			Journey: strings.Split(uq.Get("journey"), "|"),
+		}
+		var err error
+		q.Lat, err = strconv.ParseFloat(uq.Get("lat"), 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.Lng, err = strconv.ParseFloat(uq.Get("lng"), 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.Radious, err = strconv.Atoi(uq.Get("radius"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.CostRange = uq.Get("cost")
+		places := q.Run()
+		respond(w, r, places)
+	}))
+	log.Println("Starting meander API at :8080")
 	http.ListenAndServe(":8080", http.DefaultServeMux)
 }
 
@@ -20,4 +49,11 @@ func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
 		publicData[i] = meander.Public(d)
 	}
 	return json.NewEncoder(w).Encode(publicData)
+}
+
+func cors(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		f(w, r)
+	}
 }
--- src/github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/meander/query.go
+++ src/github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/meander/query.go
@@ -1,33 +1,43 @@
 package meander
 
-//AIzaSyDqiK9gdlhH0gerUhfKxnaFTRt1VTb8iyg
-//AIzaSyCZzNDxQDkfRW_oOMBeCNLoqMWAVLw5nF8
-var APIKey = "AIzaSyCZzNDxQDkfRW_oOMBeCNLoqMWAVLw5nF8"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"sync"
+	"time"
+)
 
-type Place struct {
-	*googleGeometry `json:"geometry"`
-	Name            string         `json:"name"`
-	Icon            string         `json:"icon"`
-	Photos          []*googlePhoto `json:"photos"`
-	Vicinity        string         `json:"vicinity"`
-}
+var APIKey = "AIzaSyDqiK9gdlhH0gerUhfKxnaFTRt1VTb8iyg"
 
-type googleResponse struct {
+type GoogleResponse struct {
 	Results []*Place `json:"results"`
 }
 
-type googleGeometry struct {
-	*googleLocation `json:"location"`
+type Place struct {
+	GoogleGeometry `json:"geometry"`
+	Name           string         `json:"name"`
+	Icon           string         `json:"icon"`
+	Photos         []*GooglePhoto `json:"photos"`
+	Vicinity       string         `json:"vicinity"`
 }
 
-type googleLocation struct {
+// GoogleGeometry must be Public otherwise json decode panics, same to the rest of embedded structures
+type GoogleGeometry struct {
+	*GoogleLocation `json:"location"`
+}
+
+type GoogleLocation struct {
 	Lat float64 `json:"lat"`
 	Lng float64 `json:"lng"`
 }
 
-type googlePhoto struct {
+type GooglePhoto struct {
 	PhotoRef string `json:"photo_reference"`
-	URL      string `json:"url"`
+	URL      string //`json:"url"`
 }
 
 func (p *Place) Public() interface{} {
@@ -39,4 +49,81 @@ func (p *Place) Public() interface{} {
 		"lat":      p.Lat,
 		"lng":      p.Lng,
 	}
+}
+
+type Query struct {
+	Lat       float64
+	Lng       float64
+	Journey   []string
+	Radious   int
+	CostRange string
+}
+
+func (q *Query) find(types string) (*GoogleResponse, error) {
+	u := "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+	vals := make(url.Values)
+	vals.Set("location", fmt.Sprintf("%f,%f", q.Lat, q.Lng))
+	vals.Set("radius", fmt.Sprintf("%d", q.Radious))
+	vals.Set("types", types)
+	vals.Set("key", APIKey)
+	if len(q.CostRange) > 0 {
+		r, err := ParseCostRange(q.CostRange)
+		if err != nil {
+			return nil, err
+		}
+		vals.Set("minprice", fmt.Sprintf("%d", int(r.From)-1))
+		vals.Set("maxprice", fmt.Sprintf("%d", int(r.To)-1))
+	}
+	url := u + "?" + vals.Encode()
+	//log.Println(url)
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	var response GoogleResponse
+
+	// bodyBytes, _ := ioutil.ReadAll(res.Body)
+	// bodyString := string(bodyBytes)
+	// log.Println(bodyString)
+	// os.Exit(0)
+
+	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
+
+func (q *Query) Run() []interface{} {
+	rand.Seed(time.Now().UnixNano())
+	var w sync.WaitGroup
+	var l sync.Mutex
+	places := make([]interface{}, len(q.Journey))
+	for i, r := range q.Journey {
+		w.Add(1)
+		go func(types string, i int) {
+			defer w.Done()
+			response, err := q.find(types)
+			if err != nil {
+				log.Println("Couldnt find places:", err)
+				return
+			}
+			if len(response.Results) == 0 {
+				log.Println("Found no places:", err)
+				return
+			}
+			for _, result := range response.Results {
+				for _, photo := range result.Photos {
+					photo.URL = "https://maps.googleapis.com/maps/api/place/photo?" +
+						"maxwidth=1000&photoreference=" + photo.URL + "&key=" + APIKey
+				}
+			}
+			randI := rand.Intn(len(response.Results))
+			l.Lock()
+			places[i] = response.Results[randI]
+			l.Unlock()
+		}(r, i)
+	}
+	w.Wait()
+	return places
 }
