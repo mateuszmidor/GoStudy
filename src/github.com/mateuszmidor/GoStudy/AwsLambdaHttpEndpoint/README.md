3fca2ef03ab220bb84832cb4cff704e41a28506f
Mateusz Midor <3demaniac@gmail.com>
2019-01-27 12:37:19 +0100

    Updated README.md and hello.go

--- src/github.com/mateuszmidor/GoStudy/AwsLambdaHttpEndpoint/README.md
+++ src/github.com/mateuszmidor/GoStudy/AwsLambdaHttpEndpoint/README.md
@@ -1,6 +1,7 @@
 # AwsLambdaHttpEndpoint
 This is an example AWS lambda function written in GO, to be used as HTTP endpoint.
 Notice that you need to upload to AWS a compiled GO Application, not just GO source code!
+see: https://yos.io/2018/02/08/getting-started-with-serverless-go/
 
 # Building deployment package
 go build hello.go
--- src/github.com/mateuszmidor/GoStudy/AwsLambdaHttpEndpoint/hello.go
+++ src/github.com/mateuszmidor/GoStudy/AwsLambdaHttpEndpoint/hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -9,35 +10,31 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// MyEvent will hold the incoming event json data
-type MyEvent struct {
+// NOTICE: what normally is called a Request, AWS Lambda calls an Event
+
+// EventData will hold the incoming json data
+type EventData struct {
 	Name string `json:"name"`
 }
 
-// HandleRequest will process the incoming event and return result string
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	t := time.Now()
-	tstr := t.Format("2006-01-02 15:04:05")
-	return fmt.Sprintf("Hello %s! Its %s", name.Name, tstr), nil
+// handleRequest will receive the incoming event and return result in a proper format
+func handleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// decode the event parameter
+	var data EventData
+	if err := json.Unmarshal([]byte(event.Body), &data); err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: 500}, err
+	}
+
+	// prepare the response string
+	currentTime := time.Now()
+	currentTimeStr := currentTime.Format("2006-01-02 15:04:05")
+	responseStr := fmt.Sprintf("Hello from AWS Lambda, %s! Its %s", data.Name, currentTimeStr)
+
+	// return the response
+	return events.APIGatewayProxyResponse{Body: responseStr, StatusCode: 200}, nil
 }
 
-type EmptyEvent struct {
-}
-
-type WelcomeMessage struct {
-	WelcomeMessage []byte
-}
-
-func SimpleRequestHandler(ctx context.Context, empty EmptyEvent) (events.APIGatewayProxyResponse, error) {
-	// t := time.Now()
-	// tstr := t.Format("2006-01-02 15:04:05")
-	// result := WelcomeMessage{[]byte(fmt.Sprintf("Hello! Its %s", tstr))}
-	// json_result, err := json.Marshal(result)
-	//return `{"msg" : "Hello!"}`, nil //string(json_result), err
-
-	return events.APIGatewayProxyResponse{Body: `{"msg" : "Hello!"}`, StatusCode: 200}, nil
-}
-
+// in main() we register the lambda request handler
 func main() {
-	lambda.Start(SimpleRequestHandler)
+	lambda.Start(handleRequest)
 }
