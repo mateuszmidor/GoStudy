3873b314394cb6cb46e1f4aefdf677a5f7ee1d69
Mateusz Midor <3demaniac@gmail.com>
2019-02-05 21:46:49 +0100

    Added main.go

---
+++ src/github.com/mateuszmidor/GoStudy/GoProgrammingBlueprints/backup/cmds/backup/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/matryer/filedb"
+)
+
+type path struct {
+	Path string
+	Hash string
+}
+
+func main() {
+	var fatalErr error
+	defer func() {
+		if fatalErr != nil {
+			flag.PrintDefaults()
+			log.Fatalln(fatalErr)
+		}
+	}()
+
+	var dbpath = flag.String("dg", "./backupdata", "ścieżka do katalogu bazy danych")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		fatalErr = errors.New("Need to specify command")
+		return
+	}
+
+	db, err := filedb.Dial(*dbpath)
+	if err != nil {
+		fatalErr = err
+		return
+	}
+
+	defer db.Close()
+	col, err := db.C("paths")
+	if err != nil {
+		fatalErr = err
+		return
+	}
+
+	switch strings.ToLower(args[0]) {
+	case "list":
+		var path path
+		col.ForEach(func(i int, data []byte) bool {
+			err := json.Unmarshal(data, &path)
+			if err != nil {
+				fatalErr = err
+				return true
+			}
+			fmt.Printf("= %s\n", path)
+			return false
+		})
+	case "add":
+	case "remove":
+	}
+}
