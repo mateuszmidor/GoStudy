4a9bcd4964677f3d54e07c008f4a675c2d9920cc
Mateusz Midor <3demaniac@gmail.com>
2019-07-21 19:39:14 +0200

    Minor updates to kafka

--- src/github.com/mateuszmidor/GoStudy/sdars/kafka_microservices_demo/main.go
+++ src/github.com/mateuszmidor/GoStudy/sdars/kafka_microservices_demo/main.go
@@ -1,6 +1,9 @@
 package main
 
+// BASED ON: https://medium.com/@yusufs/getting-started-with-kafka-in-golang-14ccab5fa26
 import (
+	"context"
+	"log"
 	"time"
 
 	"github.com/segmentio/kafka-go"
@@ -29,6 +32,21 @@ func Configure(kafkaBrokerUrls []string, clientId string, topic string) (w *kafk
 	return w, nil
 }
 
+func Push(parent context.Context, key, value []byte) (err error) {
+	message := kafka.Message{
+		Key:   key,
+		Value: value,
+		Time:  time.Now(),
+	}
+	return writer.WriteMessages(parent, message)
+}
+
 func main() {
 
+	Configure([]string{"localhost:19092", "localhost:29092", "localhost:39092"}, "clientId", "foo")
+
+	err := Push(context.Background(), nil, []byte("\n kafka_microservices_demo \n"))
+	if err != nil {
+		log.Fatal("Kafka write error", err)
+	}
 }
--- src/github.com/mateuszmidor/GoStudy/sdars/kafka_microservices_demo/test_kafka_consumer.sh
+++ src/github.com/mateuszmidor/GoStudy/sdars/kafka_microservices_demo/test_kafka_consumer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kafkacat -C -b localhost:19092,localhost:29092,localhost:39092 -t foo -p 0
+kafkacat -C -b localhost:19092,localhost:29092,localhost:39092 -t foo # -p 0 # READ FROM ALL PARTITIONS
