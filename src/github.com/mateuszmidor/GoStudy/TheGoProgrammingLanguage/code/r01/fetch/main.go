d5265de218069ab7090aa0344788c0394de63832
Mateusz Midor <3demaniac@gmail.com>
2020-01-28 18:19:46 +0100

    Added TheGoProgrammingLanguage r01

---
+++ src/github.com/mateuszmidor/GoStudy/TheGoProgrammingLanguage/code/r01/fetch/main.go
@@ -0,0 +1,64 @@
+package main
+
+// Project: http resource fetcher, like wget/curl
+// Usage: ./fetch http://golang.org
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	improvedFetch()
+	// for _, url := range os.Args[1:] {
+	// 	resp, err := http.Get(url)
+	// 	if err != nil {
+	// 		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+	// 		os.Exit(1)
+	// 	}
+	// 	b, err := ioutil.ReadAll(resp.Body)
+	// 	resp.Body.Close()
+	// 	if err != nil {
+	// 		fmt.Fprintf(os.Stderr, "fetch: error reading %s: %v\n", url, err)
+	// 		os.Exit(1)
+	// 	}
+	// 	fmt.Print(string(b))
+	// }
+}
+
+func improvedFetch() {
+	for _, url := range os.Args[1:] {
+		prefixedURL := addHTTPPrefixIfMissing(url)
+		resp := httpGetOrExit(prefixedURL)
+		ioCopyOrExit(os.Stdout, resp.Body)
+		fmt.Println("HTTP STATUS: ", resp.Status)
+	}
+}
+
+func httpGetOrExit(url string) *http.Response {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		os.Exit(1)
+	}
+	return resp
+}
+
+func ioCopyOrExit(dst io.Writer, src io.Reader) {
+	_, err := io.Copy(dst, src)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: error copying to stdout %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func addHTTPPrefixIfMissing(url string) string {
+	const HTTP = "http://"
+	if strings.HasPrefix(url, HTTP) {
+		return url
+	}
+	return HTTP + url
+}
---
+++ src/github.com/mateuszmidor/GoStudy/TheGoProgrammingLanguage/code/r01/server2/main.go
@@ -0,0 +1,33 @@
+package main
+
+// Project: echo server that responds with received URL OR with total number of received requests
+// Usage: ./server2 & firefox localhost:8000/MainPage.html & firefox localhost:8000/count
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Num http requests so far: %d\n", count)
+	mu.Unlock()
+}
