3939d98227f5dadc7adefd00ba04d281cd943040
Mateusz Midor <3demaniac@gmail.com>
2021-02-24 16:05:32 +0100

    Updated prettyprint to print maps as well

--- prettyprint/src/main.go
+++ prettyprint/src/main.go
@@ -6,56 +6,62 @@ import (
 	"reflect"
 )
 
-// PrettyPrint prints object in nice nested way
+// PrettyPrint prints an object recursively in an indented way
 func PrettyPrint(resource interface{}) {
-	prettyPrintRecursive(reflect.TypeOf(resource), reflect.ValueOf(resource), 0)
+	prettyPrintRecursively(reflect.TypeOf(resource), reflect.ValueOf(resource), 0)
 }
 
-func prettyPrintRecursive(t reflect.Type, v reflect.Value, level int) {
-	// sentinel for infinite recursion error
-	if level > 10 {
-		panic("Level > 10. Recursion too deep")
-	}
-
+func prettyPrintRecursively(t reflect.Type, v reflect.Value, level int) {
 	switch v.Kind() {
 
 	case reflect.Struct:
+		if _, hasStringer := t.MethodByName("String"); hasStringer {
+			prettyPrintIndented("%v\n", level, v)
+			return
+		}
 		for i := 0; i < v.NumField(); i++ {
-			printfl("%s:\n", level, t.Field(i).Name)
-			prettyPrintRecursive(v.Field(i).Type(), v.Field(i), level+1)
+			prettyPrintIndented("%s:\n", level, t.Field(i).Name)
+			prettyPrintRecursively(v.Field(i).Type(), v.Field(i), level+1)
 		}
 
 	case reflect.Slice:
 		count := v.Len()
 		if count == 0 {
-			printfl("[no items]\n", level)
+			prettyPrintIndented("[no items]\n", level)
 
 		} else {
 			for i := 0; i < count; i++ {
-				printfl("[%d]\n", level, i)
+				prettyPrintIndented("[%d]\n", level, i)
 				s := v.Index(i)
-				prettyPrintRecursive(s.Type(), s, level)
+				prettyPrintRecursively(s.Type(), s, level+1)
 			}
+		}
+
+	case reflect.Map:
+		for _, key := range v.MapKeys() {
+			prettyPrintIndented("[%s]\n", level, key)
+			s := v.MapIndex(key)
+			prettyPrintRecursively(s.Type(), s, level+1)
 		}
 
 	case reflect.Ptr, reflect.Interface:
 		if v.IsNil() {
-			printfl("[empty]\n", level)
+			prettyPrintIndented("[empty]\n", level)
 		} else {
-			prettyPrintRecursive(v.Elem().Type(), v.Elem(), level)
+			prettyPrintRecursively(v.Elem().Type(), v.Elem(), level)
 		}
 
 	default:
-		printfl("%v\n", level, v)
+		prettyPrintIndented("%v\n", level, v)
 	}
 
 }
 
-// Indented Printf
-func printfl(format string, level int, args ...interface{}) {
+func prettyPrintIndented(format string, level int, args ...interface{}) {
 	fmt.Printf("%*s", level*2, "")
 	fmt.Printf(format, args...)
 }
+
 
 func main() {
 	PrettyPrint(os.Stderr)
