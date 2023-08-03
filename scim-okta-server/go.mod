68c8745c309bd95d6b3c2c9fd314a9ba1171204a
Mateusz Midor <3demaniac@gmail.com>
2023-08-03 09:57:41 +0200

    Replace map with ordered map for consistent getall results

--- scim-okta-server/go.mod
+++ scim-okta-server/go.mod
@@ -2,10 +2,17 @@ module github.com/mateuszmidor/GoStudy/scim
 
 go 1.20
 
-require github.com/elimity-com/scim v0.0.0-20230426070224-941a5eac92f3
+require (
+	github.com/elimity-com/scim v0.0.0-20230426070224-941a5eac92f3
+	github.com/scim2/filter-parser/v2 v2.2.0
+	github.com/wk8/go-ordered-map/v2 v2.1.8
+)
 
 require (
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/di-wu/parser v0.2.2 // indirect
 	github.com/di-wu/xsd-datetime v1.0.0 // indirect
-	github.com/scim2/filter-parser/v2 v2.2.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- scim-okta-server/resources.go
+++ scim-okta-server/resources.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/elimity-com/scim"
 	scim_errors "github.com/elimity-com/scim/errors"
+	orderedmap "github.com/wk8/go-ordered-map/v2"
 )
 
 // InMemoryResourceHandler is an in-memory implementation of scim.ResourceHandler interface.
 type InMemoryResourceHandler struct {
-	resources map[string]scim.Resource // keyed by resource ID
+	resources *orderedmap.OrderedMap[string, scim.Resource] // keyed by resource ID
 }
 
 // NewMockResourceHandler creates a new instance of MockResourceHandler.
 func NewInMemoryResourceHandler(initResources []scim.Resource) *InMemoryResourceHandler {
-	resources := map[string]scim.Resource{}
+	resources := orderedmap.New[string, scim.Resource]()
 	for _, r := range initResources {
-		resources[r.ID] = r
+		resources.Set(r.ID, r)
 	}
 	return &InMemoryResourceHandler{
 		resources: resources,
@@ -26,25 +26,23 @@ func NewInMemoryResourceHandler(initResources []scim.Resource) *InMemoryResource
 }
 
 // Create stores the given attributes and returns a resource with the attributes that are stored and a unique identifier.
-func (m *InMemoryResourceHandler) Create(r *http.Request, attributes scim.ResourceAttributes) (scim.Resource, error) {
-	log.Printf("%+v", attributes)
-
+func (m *InMemoryResourceHandler) Create(_ *http.Request, attributes scim.ResourceAttributes) (scim.Resource, error) {
 	if m.isDuplicate(attributes) {
 		return scim.Resource{}, scim_errors.ScimErrorUniqueness
 	}
 
-	id := fmt.Sprintf("resource%d", len(m.resources)+1)
+	id := fmt.Sprintf("resource%d", m.resources.Len()+1)
 	resource := scim.Resource{
 		ID:         id,
 		Attributes: attributes,
 	}
-	m.resources[id] = resource
+	m.resources.Set(id, resource)
 	return resource, nil
 }
 
 // Get returns the resource corresponding to the given identifier.
-func (m *InMemoryResourceHandler) Get(r *http.Request, id string) (scim.Resource, error) {
-	if resource, ok := m.resources[id]; ok {
+func (m *InMemoryResourceHandler) Get(_ *http.Request, id string) (scim.Resource, error) {
+	if resource, ok := m.resources.Get(id); ok {
 		return resource, nil
 	}
 
@@ -52,103 +50,68 @@ func (m *InMemoryResourceHandler) Get(r *http.Request, id string) (scim.Resource
 }
 
 // GetAll returns a paginated list of resources.
-func (m *InMemoryResourceHandler) GetAll(r *http.Request, params scim.ListRequestParams) (scim.Page, error) {
-	// sanity check
+func (m *InMemoryResourceHandler) GetAll(_ *http.Request, params scim.ListRequestParams) (scim.Page, error) {
+	// sanitize input
 	if params.StartIndex < 1 {
 		params.StartIndex = 1
 	}
-	numResoures := len(m.resources)
-	if params.StartIndex > numResoures {
-		msg := fmt.Sprintf("StartIndex (%d) > total item count (%d)", params.StartIndex, numResoures)
-		return scim.Page{}, scim_errors.ScimErrorBadParams([]string{msg})
-	}
 
-	// handle special case
+	// handle special case - for Count <= 0 return only the total number of resources and no actual resources
 	if params.Count <= 0 {
-		return scim.Page{Resources: nil, TotalResults: numResoures}, nil
+		return scim.Page{Resources: nil, TotalResults: m.resources.Len()}, nil
 	}
 
-	// filter matching resources
-	first := params.StartIndex
-	last := first + params.Count // TODO: this is probably wrong as we are filtering the items
-	if last > numResoures {
-		last = numResoures
-	}
-	var resources []scim.Resource
-	var i int // 1-based index
-	for _, resource := range m.resources {
-		i++
-		if i < first {
+	// collect resources matching requested criteria
+	var index int // 1-based item index
+	var results []scim.Resource
+	for pair := m.resources.Oldest(); pair != nil; pair = pair.Next() {
+		index++
+		if index < params.StartIndex {
 			continue
 		}
-		if i > last {
+		if len(results) == params.Count {
 			break
 		}
-		// match, err := filter.EvalExpression(resource.Attributes, params.Filter)
-		// if err != nil {
-		// 	return scim.Page{}, scim_errors.ScimErrorBadRequest(err.Error())
-		// }
-		// if !match {
-		// 	continue
-		// }
-		resources = append(resources, resource)
+
+		results = append(results, pair.Value)
 	}
 	return scim.Page{
-		Resources:    resources,
-		TotalResults: len(resources),
+		Resources:    results,
+		TotalResults: len(results),
 	}, nil
 }
 
 // Replace replaces ALL existing attributes of the resource with the given identifier.
-func (m *InMemoryResourceHandler) Replace(r *http.Request, id string, attributes scim.ResourceAttributes) (scim.Resource, error) {
-	log.Printf("%s - %+v", id, attributes)
-	if _, ok := m.resources[id]; !ok {
+func (m *InMemoryResourceHandler) Replace(_ *http.Request, id string, attributes scim.ResourceAttributes) (scim.Resource, error) {
+	if _, ok := m.resources.Get(id); !ok {
 		return scim.Resource{}, scim_errors.ScimErrorResourceNotFound(id)
 	}
 	resource := scim.Resource{
 		ID:         id,
 		Attributes: attributes,
 	}
-	m.resources[id] = resource
+	m.resources.Set(id, resource)
 	return resource, nil
 }
 
 // Delete removes the resource with the corresponding ID.
-func (m *InMemoryResourceHandler) Delete(r *http.Request, id string) error {
-	if _, ok := m.resources[id]; !ok {
+func (m *InMemoryResourceHandler) Delete(_ *http.Request, id string) error {
+	if _, ok := m.resources.Get(id); !ok {
 		return scim_errors.ScimErrorResourceNotFound(id)
 	}
-	delete(m.resources, id)
+	m.resources.Delete(id) // it's ok if resource not present
 	return nil
 }
 
 // Patch updates one or more attributes of a SCIM resource using a sequence of operations.
-func (m *InMemoryResourceHandler) Patch(r *http.Request, id string, operations []scim.PatchOperation) (scim.Resource, error) {
-	log.Printf("%s - %+v", id, operations)
+func (m *InMemoryResourceHandler) Patch(_ *http.Request, id string, operations []scim.PatchOperation) (scim.Resource, error) {
 	return scim.Resource{}, scim_errors.ScimError{Detail: "Patch is not implemented", Status: http.StatusNotImplemented}
-
-	// if _, ok := m.resources[id]; !ok {
-	// 	return scim.Resource{}, scim_errors.ScimErrorResourceNotFound(id)
-	// }
-
-	// resource := m.resources[id]
-	// for _, op := range operations {
-	// 	switch op.Op {
-	// 	case "add", "replace":
-	// 		resource.Attributes[op.Path.AttributePath.String()] = op.Value
-	// 	case "remove":
-	// 		delete(resource.Attributes, op.Path.String())
-	// 	}
-	// }
-
-	// m.resources[id] = resource
-	// return resource, nil
 }
 
 func (m *InMemoryResourceHandler) isDuplicate(attributes scim.ResourceAttributes) bool {
-	for _, r := range m.resources {
-		// only userName for User resource needs to be unique, according to schema.CoreUserSchema()
-		if attributes["userName"] != nil && r.Attributes["userName"] == attributes["userName"] {
+	for pair := m.resources.Oldest(); pair != nil; pair = pair.Next() {
+		// only userName for User resource needs to be unique, according to schema.CoreUserSchema() and schema.CoreGroupSchema()
+		if pair.Value.Attributes["userName"] != nil && pair.Value.Attributes["userName"] == attributes["userName"] {
 			return true
 		}
 	}
