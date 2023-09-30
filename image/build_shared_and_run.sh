7e2c00e493572de74ce77386ad6ceb8ec959a8e4
Mateusz Midor <3demaniac@gmail.com>
2023-09-30 17:57:24 +0200

    Move image/ under root/

---
+++ image/build_shared_and_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Building golang libimgutil.so library..."
+go build -o libimgutil.so -buildmode=c-shared -ldflags="-s -w"  github.com/mateuszmidor/GoStudy/image/
+
+echo "Building c++ test app..."
+g++ test.cpp -o test_shared -L . -limgutil
+
+echo "Running test app with ball.jpg param..."
+export LD_LIBRARY_PATH=.
+./test_shared ball.jpg
---
+++ image/go.mod
@@ -0,0 +1,3 @@
+module github.com/mateuszmidor/GoStudy/image
+
+go 1.20
