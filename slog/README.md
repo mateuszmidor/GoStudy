2c62ba3831fce8391a045e7bd54e839842762494
Mateusz Midor <3demaniac@gmail.com>
2023-10-14 10:24:43 +0200

    improve slog/

--- slog/README.md
+++ slog/README.md
@@ -16,12 +16,12 @@ slog.Info("hello world!", "field1", 42) // field1=42
 
 ## text format
 
-```text
-time=2023-10-08T09:38:08.844+02:00 level=DEBUG msg="hello world!" field1=42
+```sh
+time=2023-10-08T10:18:08.170+02:00 level=DEBUG source=/home/user/SoftwareDevelopment/GoStudy/slog/main.go:22 msg="hello world!" field1=42
 ```
 
 ## json format
 
 ```json
-{"time":"2023-10-08T09:42:43.184688767+02:00","level":"DEBUG","msg":"hello world!","field1":42}
+{"time":"2023-10-08T10:18:08.170959432+02:00","level":"DEBUG","source":{"function":"main.main","file":"/home/user/SoftwareDevelopment/GoStudy/slog/main.go","line":28},"msg":"hello world!","field1":42}
 ```
--- slog/main.go
+++ slog/main.go
@@ -6,24 +6,23 @@ import (
 	"os"
 )
 
-var logOptions = slog.HandlerOptions{Level: slog.LevelDebug}
+var logOptions = slog.HandlerOptions{Level: slog.LevelDebug, AddSource: true} // AddSource to include file and line number
 
 func main() {
+	// simplest example; default log level is Info, and no lower level will be recorded
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lmsgprefix | log.LUTC | log.Lshortfile)
 	log.SetPrefix("[slog-demo] ")
-
-	// simplest example; default log level is Info, and no lower level will be recorded
 	slog.Info("hello world!", "field1", 42) // 2023/10/08 07:36:06.049905 main.go:15: [slog-demo] INFO hello world! field1=42
 
 	// output log in text format, enable DEBUG level
 	textTandler := slog.NewTextHandler(os.Stdout, &logOptions)
 	textLogger := slog.New(textTandler)
 	slog.SetDefault(textLogger)
-	slog.Debug("hello world!", "field1", 42) // time=2023-10-08T09:38:08.844+02:00 level=DEBUG msg="hello world!" field1=42
+	slog.Debug("hello world!", "field1", 42) // time=2023-10-08T10:18:08.170+02:00 level=DEBUG source=/home/user/SoftwareDevelopment/GoStudy/slog/main.go:22 msg="hello world!" field1=42
 
 	// output log in json format, enable DEBUG level
 	jsonHandler := slog.NewJSONHandler(os.Stdout, &logOptions)
 	jsonLogger := slog.New(jsonHandler)
 	slog.SetDefault(jsonLogger)
-	slog.Debug("hello world!", "field1", 42) // {"time":"2023-10-08T09:42:43.184688767+02:00","level":"DEBUG","msg":"hello world!","field1":42}
+	slog.Debug("hello world!", slog.Int("field1", 42)) // {"time":"2023-10-08T10:18:08.170959432+02:00","level":"DEBUG","source":{"function":"main.main","file":"/home/user/SoftwareDevelopment/GoStudy/slog/main.go","line":28},"msg":"hello world!","field1":42}
 }
