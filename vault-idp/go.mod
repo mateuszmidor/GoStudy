c601f66d06da1fac943a99a08dda6faa05ca1322
Mateusz Midor <3demaniac@gmail.com>
2024-01-08 12:50:12 +0100

    Programatically configure Vault as IDP

---
+++ vault-idp/go.mod
@@ -0,0 +1,22 @@
+module github.com/mateuszmidor/GoStudy/vault
+
+go 1.20
+
+require (
+	github.com/hashicorp/vault-client-go v0.3.3
+	github.com/mateuszmidor/pretty/v2 v2.0.0
+)
+
+require (
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.1 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/go-secure-stdlib/strutil v0.1.2 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/ryanuber/go-glob v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20220921164117-439092de6870 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/time v0.0.0-20220922220347-f3bd1da661af // indirect
+)
---
+++ vault-idp/main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/url"
+	"time"
+
+	"github.com/hashicorp/vault-client-go"
+	"github.com/hashicorp/vault-client-go/schema"
+)
+
+var userName = "user"
+var userPass = "pass"
+var providerName = "my-provider"
+var webappName = "my-webapp"
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+	ctx := context.Background()
+
+	// prepare a client with the given base address
+	client, err := vault.New(
+		vault.WithAddress("http://127.0.0.1:8200"),
+		vault.WithRequestTimeout(30*time.Second),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// authenticate with a root token (insecure)
+	if err := client.SetToken("root-token"); err != nil {
+		log.Fatal(err)
+	}
+
+	// // create/get user
+	// ereq := schema.EntityCreateRequest{
+	// 	Name: userName,
+	// }
+	// entity, err := client.Identity.EntityCreate(ctx, ereq)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// if entity == nil {
+	// 	entity, err = client.Identity.EntityReadByName(ctx, userName)
+	// }
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// secReq := schema.MountsEnableSecretsEngineRequest{
+	// 	Type: "passthrough",
+	// }
+	// _, err = client.System.MountsEnableSecretsEngine(ctx, "auth", secReq)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	req := schema.UserpassWriteUserRequest{
+		Password: userPass,
+	}
+	vals := url.Values{}
+	vals.Add("token_ttl", "60")
+	vals.Add("token_max_ttl", "60")
+	vals.Add("token_explicit_max_ttl", "60")
+	opts := vault.WithCustomQueryParameters(vals)
+	_, err = client.Auth.UserpassWriteUser(ctx, userName, req, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := client.Auth.UserpassLogin(ctx, userName, schema.UserpassLoginRequest{Password: userPass}, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", *res.Auth)
+
+	// crate OIDC client app
+	clientReq := schema.OidcWriteClientRequest{
+		RedirectUris:   []string{"http://localhost:8000/auth/callback"},
+		Assignments:    []string{"allow_all"},
+		Key:            "default",
+		IdTokenTtl:     300,
+		AccessTokenTtl: 3600,
+	}
+	_, err = client.Identity.OidcWriteClient(ctx, webappName, clientReq)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create OIDC provider
+	providerReq := schema.OidcWriteProviderRequest{
+		AllowedClientIds: []string{"*"},
+		// ScopesSupported: []string,
+	}
+	_, err = client.Identity.OidcWriteProvider(ctx, providerName, providerReq)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// print oidc client app credentials to use
+	mywebapp, err := client.Identity.OidcReadClient(ctx, webappName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("ClientID:", mywebapp.Data["client_id"])
+	fmt.Println("ClientSecret:", mywebapp.Data["client_secret"])
+
+	// print oidc issuer URL to use
+	provider, err := client.Identity.OidcReadProvider(ctx, providerName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("IssuerURL:", provider.Data["issuer"])
+
+	// add ALIAS. is this needed?
+	// fmt.Printf("%+v\n", entity)
+	// entityID := entity.Data["id"].(string)
+	// fmt.Println("entityID:", entityID)
+	// areq := schema.EntityCreateAliasRequest{
+	// 	CanonicalId: entityID,
+	// 	// Id:            entityID,
+	// 	MountAccessor: "auth_userpass_97588e92",
+	// 	Name:          name,
+	// }
+	// _, err = client.Identity.EntityCreateAlias(ctx, areq)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+}
