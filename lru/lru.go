4df42ebcb4c9949d9a7f17ddb8124db7d59d7d15
Mateusz Midor <3demaniac@gmail.com>
2025-10-10 13:33:37 +0200

    optimize lookup times in lru

--- lru/lru.go
+++ lru/lru.go
@@ -1,60 +1,55 @@
 package main
 
-import "time"
+import (
+	"container/list"
+)
 
-type KeyType string
+type KeyType = string
 type ValueType = string
-type Value struct {
-	Val  ValueType
-	date time.Time
+
+type CacheEntry struct {
+	Key KeyType
+	Val ValueType
 }
 
 type LRU struct {
-	maxSize int
-	items   map[KeyType]Value
+	maxSize  int                       // cache is limited in size
+	items    map[KeyType]*list.Element // map items store pointers to Elements in tracking list; fast lookup by key
+	tracking *list.List                // tracking list Elements hold instances of *CacheEntry; fast lookup by who was least recently used
 }
 
 func NewLRU(size int) *LRU {
-	return &LRU{maxSize: size, items: make(map[KeyType]Value)}
+	return &LRU{
+		maxSize:  size,
+		items:    make(map[KeyType]*list.Element),
+		tracking: list.New(),
+	}
 }
 
 func (lru *LRU) Get(key KeyType) *ValueType {
-	val, ok := lru.items[key]
+	element, ok := lru.items[key]
 	if !ok {
 		return nil
 	}
-	newVal := Value{Val: val.Val, date: val.date}
-	lru.items[key] = newVal
-	return &val.Val
+	lru.tracking.MoveToFront(element)
+	entry := element.Value.(*CacheEntry) // list element holds instance of *CacheEntry
+	return &entry.Val
 }
 
 func (lru *LRU) Put(key KeyType, val ValueType) {
+	// cache size limit reached; we need to kick out someone first
 	if len(lru.items) == lru.maxSize {
-		lru.DiscarLRU()
+		lruElement := lru.tracking.Back()          // find least recently used element
+		lruEntry := lruElement.Value.(*CacheEntry) // list element holds instance of *CacheEntry
+		delete(lru.items, lruEntry.Key)            // remove it from the item map
+		lru.tracking.Remove(lruElement)            // remove it from the tracking list
 	}
-	newVal := Value{Val: val, date: time.Now()}
-	lru.items[key] = newVal
+
+	newEntry := &CacheEntry{Key: key, Val: val}
+	newElement := lru.tracking.PushFront(newEntry) // insert new element into the tracking list
+	lru.items[key] = newElement                    // insert new element into the items map
 }
 
 func (lru *LRU) Size() int {
 	return len(lru.items)
-}
-
-func (lru *LRU) DiscarLRU() {
-	discardCandidateKey := lru.FindDiscardCandiate()
-	if discardCandidateKey != nil {
-		delete(lru.items, *discardCandidateKey)
-	}
-}
-
-func (lru *LRU) FindDiscardCandiate() *KeyType {
-	oldest := time.Now()
-	var key *KeyType
-	for k, v := range lru.items {
-		if v.date.Before(oldest) {
-			oldest = v.date
-			key = &k
-		}
-	}
-	return key
 }
