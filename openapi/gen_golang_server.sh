a4a9c7d360095d0e4222ddec5d6cf3a16604fb02
Mateusz Midor <3demaniac@gmail.com>
2026-04-01 18:43:38 +0200

    remove old ./openapi

--- openapi/gen_golang_server.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-output="generated_server/"
-docker run --rm  -v `pwd`:/local openapitools/openapi-generator-cli generate  -i /local/fridge_api.yaml -g go-server -o /local/$output
-sudo chown  -R $USER:$USER  $output # by default the generated code is in user:group root:root
--- openapi/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/mateuszmidor/GoStudy/openapi
-
-go 1.13
-
-require (
-	github.com/gorilla/mux v1.7.3
-	golang.org/x/oauth2 v0.0.0-20210220000619-9bb904979d93
-)
