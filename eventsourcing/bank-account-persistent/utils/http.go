9ab1c82b34ccce86ede119b4dad5d60b71065e5e
Mateusz Midor <3demaniac@gmail.com>
2026-07-19 19:21:12 +0200

    log http response body on error

--- eventsourcing/bank-account-persistent/utils/http.go
+++ eventsourcing/bank-account-persistent/utils/http.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"bytes"
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 type statusWriter struct {
 	http.ResponseWriter
 	statusCode int
+	body       bytes.Buffer
 }
 
 func (sw *statusWriter) WriteHeader(code int) {
@@ -16,12 +18,21 @@ func (sw *statusWriter) WriteHeader(code int) {
 	sw.ResponseWriter.WriteHeader(code)
 }
 
+func (sw *statusWriter) Write(b []byte) (int, error) {
+	sw.body.Write(b)
+	return sw.ResponseWriter.Write(b)
+}
+
 func NewRequestLogger(mux http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		sw := &statusWriter{ResponseWriter: w, statusCode: http.StatusOK}
 		mux.ServeHTTP(sw, r)
 		paddedMethod := fmt.Sprintf("%4s", r.Method)
-		s := fmt.Sprintf("[%d] %s %s", sw.statusCode, paddedMethod, r.URL.Path)
-		slog.Info(s)
+		reqStatus := fmt.Sprintf("[%d] %s %s", sw.statusCode, paddedMethod, r.URL.Path)
+		if sw.statusCode >= 400 {
+			slog.Error(reqStatus, "body", sw.body.String())
+		} else {
+			slog.Info(reqStatus)
+		}
 	})
 }
