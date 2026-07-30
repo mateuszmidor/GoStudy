a08080de03ac2239f932c6601754a3eb5dafac08
Mateusz Midor <3demaniac@gmail.com>
2026-07-30 14:10:50 +0200

    Add PublishAndRunInTransaction func

--- resiliency/outbox-pattern/outbox/publisher.go
+++ resiliency/outbox-pattern/outbox/publisher.go
@@ -21,3 +21,15 @@ func (n *Publisher) Publish(ctx context.Context, msg *Message) error {
 		return PersistMessage(ctx, msg, tx)
 	})
 }
+
+// PublishAndRunInTransaction persists the message and runs the function in a transaction
+// so it guarantees all or nothing semantics.
+func (n *Publisher) PublishAndRunInTransaction(ctx context.Context, msg *Message, fn func(ctx context.Context, tx *sql.Tx) error) error {
+	persistAndRun := func(tx *sql.Tx) error {
+		if err := PersistMessage(ctx, msg, tx); err != nil {
+			return err
+		}
+		return fn(ctx, tx)
+	}
+	return RunInTransaction(ctx, n.db, persistAndRun)
+}
